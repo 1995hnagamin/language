d83a3a8f44a40d044f74c365003ab9994a9f152c
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2017-08-03 17:53:33 +0900

    Fix memicomp in section2.cpp
    memicmp("", "", 10) != 0. Fix it.

--- cpp/2017/exceptional-cpp/section2.cpp
+++ cpp/2017/exceptional-cpp/section2.cpp
@@ -3,17 +3,17 @@
 
 int memicmp(char const *s1, char const *s2, size_t const n) {
   assert(s1 && s2);
-  for (size_t i = 0; i < n && *s1 != '\0' && *s2 != '\0'; ++i, ++s1, ++s2) {
+  size_t i = 0;
+  for (; i < n && *s1 && *s2; ++i, ++s1, ++s2) {
     char const c1 = toupper(*s1), c2 = toupper(*s2);
     if (c1 != c2) {
       return c1 < c2 ? -1 : 1;
     }
   }
-  if (*s1 == '\0' && *s2 == '\0') {
-    return 0;
-  } else {
-    return toupper(*s1) < toupper(*s2) ? -1 : 1;
+  if ((*s1 || *s2) && i < n) {
+    return (*s2) ? -1 : 1;
   }
+    return 0;
 }
 
 struct ci_char_traits : public std::char_traits<char> {
