d93fbe7246b746a6611d02012b4e029b370bdfd2
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2017-08-03 16:59:53 +0900

    Update  section2.cpp

--- cpp/2017/exceptional-cpp/section2.cpp
+++ cpp/2017/exceptional-cpp/section2.cpp
@@ -1,24 +1,49 @@
 #include <cassert>
-#include <cctype>
 #include <string>
 
-class ci_string {
-  public:
-  ci_string(const std::string& str) : value(str) {
-    for(auto&& c : value) {
-      c = std::tolower(c);
+int memicmp(char const *s1, char const *s2, size_t const n) {
+  assert(s1 && s2);
+  for (size_t i = 0; i < n && *s1 != '\0' && *s2 != '\0'; ++i, ++s1, ++s2) {
+    char const c1 = toupper(*s1), c2 = toupper(*s2);
+    if (c1 != c2) {
+      return c1 < c2 ? -1 : 1;
     }
   }
-  operator std::string() {
-    return value;
+  if (*s1 == '\0' && *s2 == '\0') {
+    return 0;
+  } else {
+    return toupper(*s1) < toupper(*s2) ? -1 : 1;
   }
-  private:
-  std::string value;
+}
+
+struct ci_char_traits : public std::char_traits<char> {
+  static bool eq(char c1, char c2) {
+    return toupper(c1) == toupper(c2);
+  }
+  static bool lt(char c1, char c2) {
+    return toupper(c1) < toupper(c2);
+  }
+  static int compare(char const *s1, char const *s2, size_t n) {
+    return memicmp(s1, s2, n);
+  }
+
+  static char const *find(char const *s, int n, char c) {
+    while (n-- > 0 && toupper(*s) != toupper(c)) {
+      ++s;
+    }
+    return n > 0 ? s : 0;
+  }
 };
+
+typedef std::basic_string<char, ci_char_traits> ci_string;
 
 int main() {
   ci_string s("AbCdE");
   assert(s == "ABCDE");
   assert(s == "abcde");
+  assert(s < "AbCdEf");
+  assert("AbCdEf" > s);
+  assert(s > "");
+  assert("" < s);
   return 0;
 }
