fdaa005f357e3fdf7ccd81a7ee0115535f7e0e5e
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2017-08-03 16:18:27 +0900

    Add answer to section1.cpp

--- cpp/2017/exceptional-cpp/section1.cpp
+++ cpp/2017/exceptional-cpp/section1.cpp
@@ -13,22 +13,25 @@ Date TodaysDate() {
 int main() {
   std::vector<Date> e;
   std::copy(std::istream_iterator<Date>(std::cin),
-    std::istream_iterator<Date>(), // ??
+    std::istream_iterator<Date>(),
     std::back_inserter(e));
   std::vector<Date>::iterator first =
     std::find(e.begin(), e.end(), "01/01/95");
   std::vector<Date>::iterator last =
     std::find(e.begin(), e.end(), "12/31/95");
-  *last = "12/31/95"; // M[last == e.end()]
+  *last = "12/31/95"; // M[last == e.end()] : OK
   std::copy(
     first,
     last,
     std::ostream_iterator<Date>(std::cout, "\n"));
+    // ~L[first < last]
   e.insert(--e.end(), TodaysDate());
+    // M[Illegal(`--e.end()`)]
+    // M[e.empty()]
   std::copy(
     first,
     last,
     std::ostream_iterator<Date>(std::cout, "\n"));
-
+    // ~L[Valid(first), Valid(last)]
   return 0;
 }
