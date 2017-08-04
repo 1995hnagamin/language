8b829d865d583296d99c6b91d5114d5db6aeac8e
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2017-08-04 17:08:01 +0900

    Read section 9

--- cpp/2017/exceptional-cpp/section8.cpp
+++ cpp/2017/exceptional-cpp/section8.cpp
@@ -1,15 +1,18 @@
 #include <algorithm>
 #include <cassert>
 #include <cstdlib>
+#include <string>
 
 template <class T>
 T *NewCopy(T const *src, size_t srcsize, size_t dstsize) {
   assert(dstsize >= srcsize);
   T *dst = new T[dstsize];
+    // `operator new[]` may throw std::bad_alloc
+    // `T::T` may throw some exceptions
   try {
-    std::copy(src, src + dst, dst);
+    std::copy(src, src + srcsize, dst);
   } catch (...) {
-    delete[] dst;
+    delete[] dst; // don't throw exceptions
     throw;
   }
   return dst;
@@ -27,16 +30,19 @@ class Stack {
       delete[] v_;
     }
     Stack(Stack const &other):
-      v_(new T[other.vsize_]),
+      v_(NewCopy(other.v_, other.vsize_, other.vsize_)),
       vsize_(other.vsize_),
       vused_(other.vused_)
-    {
-      for (size_t i = 0; i < vsize_; ++i) {
-        v_[i] = other.v_[i];
-      }
-    }
+    {}
     Stack &operator=(Stack const &rhs) {
-      Swap(rhs);
+      if (this != &rhs) {
+        T *v_new = NewCopy(rhs.v_, rhs.vsize_, rhs.vsize_);
+          // may throw
+        delete[] v_;
+        v_ = v_new;
+        vsize_ = rhs.vsize_;
+        vused_ = rhs.vused_;
+      }
       return *this;
     }
 
@@ -45,13 +51,12 @@ class Stack {
     T *v_;
     size_t vsize_;
     size_t vused_;
-    void Swap(Stack const &other) {
-      using std::swap;
-      swap(v_, other.v_);
-      swap(vsize_, other.vsize_);
-      swap(vused_, other.vused_);
-    }
 };
 
 int main() {
+  Stack<std::string> s;
+  Stack<std::string> t = s;
+  Stack<std::string> u;
+  s = u;
+  return 0;
 }
