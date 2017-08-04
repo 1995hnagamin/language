5aa1ab1bf5505058efec1199d6691b96bbfcda42
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2017-08-04 16:49:44 +0900

    Add copy-ctor & operator= to Stack<T>

--- cpp/2017/exceptional-cpp/section8.cpp
+++ cpp/2017/exceptional-cpp/section8.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cstdlib>
 
 template <class T>
@@ -11,12 +12,31 @@ class Stack {
     ~Stack() {
       delete[] v_;
     }
+    Stack(Stack const &other):
+      v_(new T[other.vsize_]),
+      vsize_(other.vsize_),
+      vused_(other.vused_)
+    {
+      for (size_t i = 0; i < vsize_; ++i) {
+        v_[i] = other.v_[i];
+      }
+    }
+    Stack &operator=(Stack const &rhs) {
+      Swap(rhs);
+      return *this;
+    }
 
   private:
     static size_t const initial_stack_size = 10;
     T *v_;
     size_t vsize_;
     size_t vused_;
+    void Swap(Stack const &other) {
+      using std::swap;
+      swap(v_, other.v_);
+      swap(vsize_, other.vsize_);
+      swap(vused_, other.vused_);
+    }
 };
 
 int main() {
