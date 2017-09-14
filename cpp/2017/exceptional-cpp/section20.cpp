a7605f6f0e320a967da642403e109ad0e2875dc6
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2017-09-14 14:53:23 +0900

    Polish `class Complex` in section20.cpp

--- cpp/2017/exceptional-cpp/section20.cpp
+++ cpp/2017/exceptional-cpp/section20.cpp
@@ -1,18 +1,15 @@
 class Complex
 {
 public:
-  Complex(double real, double imaginary = 0)
-    : _real(real), _imaginary(imaginary)
+  explicit Complex(double real, double imaginary = 0)
+    : real_(real), imaginary_(imaginary)
   {
   }
-  void operator+(Complex other)
+  Complex &operator+=(const Complex &other)
   {
-    _real = _real + other.real;
-    _imaginary = _imaginary + other._imaginary
-  }
-  void operator<<(ostream os)
-  {
-    os << "(" << _real << "," << _imaginary << ")";
+    real_ = other.real_;
+    imaginary_ = other.imaginary_;
+    return *this;
   }
   Complex operator++()
   {
@@ -21,19 +18,44 @@ public:
   }
   Complex operator++(int)
   {
-    Complex temp = *this;
-    ++_real;
+    Complex temp(*this);
+    ++(*this);
     return temp;
   }
+  ostream &Print(ostream &os) const
+  {
+    return os << "(" << real_ << ", " << imaginary_ << ")";
+  }
 private:
-  double _real, _imaginary;
+  double real_;
+  double imaginary_;
 };
 
+const Complex operator+(const Complex &lhs, const Complex &rhs)
+{
+  Complex ret(lhs);
+  ret += rhs;
+  return ret;
+}
+
+ostream &operator<<(ostream &os, const Complex &c)
+{
+  return c.Print(os);
+}
+
 /*
- * [ ] メンバー名がアンダースコアから始まっている
- * [ ] `operator+` が引数を値渡ししている
- * [ ] `operator+` の返り値型がvoid
- * [ ] `operator<<` が引数のストリームを値渡ししている
- * [ ] `operator<<` の返り値型がvoid
- * [ ] インクリメント演算子が参照ではなく値を返している
+ * [o] メンバー名がアンダースコアから始まっている
+ * [o] `operator+` が引数を値渡ししている
+ * [o] `operator+` の返り値型がvoid
+ * [o] `operator<<` が引数のストリームを値渡ししている
+ * [o] `operator<<` の返り値型がvoid
+ * [o] インクリメント演算子が参照ではなく値を返している
+ *     `double`からの暗黙の型変換が存在する
+ *     @=ではなく@を使っている
+ *     `operator+`がメンバー関数
+ *     `operator+`がインスタンスの状態を変更している
+ *     `operator+`があるのに`operator+=`がない
+ *     `operator<<`がメンバー関数
+ *     後置インクリメントが非const値を返している
+ *     後置インクリメントが前置インクリメントを使わずに定義されている
  */
