9d8b2bb99181dbf247c943cffa70b07070312cb1
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2017-09-26 17:51:57 +0900

    Update exceptional-cpp/section23.cpp

--- cpp/2017/exceptional-cpp/section23.cpp
+++ cpp/2017/exceptional-cpp/section23.cpp
@@ -1,19 +1,34 @@
 #include <string>
 
+class GTAClient;
+
 class GenericTableAlgorithm {
   public:
-    GenericTableAlgorithm(const std::string &table);
-    virtual ~GenericTableAlgorithm();
+    GenericTableAlgorithm(const std::string &table, GTAClient &worker);
+    ~GenericTableAlgorithm();
     bool Process();
 
   private:
-    virtual bool Filter(const Record &);
-
-    virtual bool ProcessRow(const PrimaryKey &) = 0;
-
     class GenericTableAlgorithmImpl *pimpl_;
 };
 
-class MyAlgorithm : public GenericTableAlgorithm {
-
+class GTAClient {
+  public:
+    virtual ~GTAClient() = 0;
+    virtual bool Filter(const Record &);
+    virtual book ProcessRow(const PrimaryKey &) = 0;
 };
+
+bool GTAClient::Filter(const Record &) {
+  return true;
+}
+
+class MyWorker : public GTAClient {
+  // ...
+};
+
+int main() {
+  GenericTableAlgorithm a("Customer", MyWorker());
+  a.Process();
+  return 0;
+}
