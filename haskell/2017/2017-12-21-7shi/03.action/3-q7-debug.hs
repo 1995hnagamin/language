392068be71387ffaf2698b493c6c4ab22dde3c3b
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2017-12-24 01:04:11 +0900

    Solve 03.action/Q7

--- haskell/2017/2017-12-21-7shi/03.action/3-q7-debug.hs
+++ haskell/2017/2017-12-21-7shi/03.action/3-q7-debug.hs
@@ -1,12 +1,15 @@
 import Debug.Trace
 
-qsort [] = []
-qsort (n:xs) = trace dbg $ qsort lt ++ [n] ++ qsort gteq
-    where
-        dbg = "qsort " ++ show (n:xs) ++ " = qsort " ++
-              show lt ++ " ++ " ++ show [n] ++ " ++ " ++ show gteq
-        lt = [x | x <- xs, x < n]
-        gteq = [x | x <- xs, x >= n]
+qsort [] = do
+    return []
+qsort xs@(x:xs') = do
+    let lt = filter (<x) xs'
+    let ge = filter (>=x) xs'
+    putStrLn $ "qsort " ++ show xs ++ " = qsort " ++
+                show lt ++ " ++ " ++ show [x] ++ " ++ qsort " ++ show ge
+    lt' <- qsort lt
+    ge' <- qsort ge
+    return $ lt' ++ [x] ++ ge'
 
 main = do
-    traceIO $ show $ qsort [4, 6, 9, 8, 3, 5, 1, 7, 2]
+    print =<< qsort [4, 6, 9, 8, 3, 5, 1, 7, 2]
