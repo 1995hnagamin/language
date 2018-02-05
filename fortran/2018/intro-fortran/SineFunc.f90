78fc8db8976343e7ee9c1b7468d7953e5e469307
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2018-02-05 18:08:50 +0900

    Add SineFunc.f90

---
+++ fortran/2018/intro-fortran/SineFunc.f90
@@ -0,0 +1,10 @@
+program SineFunction
+        implicit none
+        real(8) x, y
+        x = 0
+        do while (x < 2*3.14)
+                y = sin(x)
+                print *, x, y
+                x = x + 0.1
+        end do
+end program SineFunction
