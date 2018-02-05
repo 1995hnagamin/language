f91a1a3cbaf5e977460b6c73c0630a25500de850
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2018-02-05 17:34:06 +0900

    Use only one variable

--- fortran/2018/intro-fortran/VariableTest.f90
+++ fortran/2018/intro-fortran/VariableTest.f90
@@ -1,10 +1,10 @@
 program VariableTest
         implicit none
-        real(8) x, y, z
+        real(8) x
         x = 1
         print *, x
-        y = x/2 + 1/x
-        print *, y
-        z = y/2 + 1/y
-        print *, z
+        x = x/2 + 1/x
+        print *, x
+        x = x/2 + 1/x
+        print *, x
 end program VariableTest
