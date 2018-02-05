7a6ee91f93ca8f36dbe071bd71089f389fb08330
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2018-02-05 18:03:03 +0900

    Print `spacing(x0)`

--- fortran/2018/intro-fortran/repeating.f90
+++ fortran/2018/intro-fortran/repeating.f90
@@ -1,11 +1,11 @@
 program Repeating
         implicit none
-        real(8) :: x = 1, x0 = 0, a = 10
+        real(16) :: x = 1, x0 = 0, a = 1729
         integer :: c = 0
         do while (abs(x - x0) > spacing(x0)*2)
                 c = c + 1
                 x0 = x
                 x = (x + a/x)/2
-                print *, c, x
+                print *, c, x, spacing(x0)
         end do
 end program Repeating
