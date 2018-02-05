0d594351c1da5bfa2eef3101e702892c95e98efa
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2018-02-05 18:15:41 +0900

    Use double precision literal (SineFunc.f90)

--- fortran/2018/intro-fortran/SineFunc.f90
+++ fortran/2018/intro-fortran/SineFunc.f90
@@ -5,6 +5,6 @@ program SineFunction
         do while (x < 2*3.14)
                 y = sin(x)
                 print *, x, y
-                x = x + 0.1
+                x = x + 1d-1
         end do
 end program SineFunction
