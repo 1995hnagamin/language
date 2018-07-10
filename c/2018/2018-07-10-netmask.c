a5da68d92d1a16d281d86f21d814ceaebb76b73c
NAGAMINE Hideaki <hnagamin@trans-nt.com>
2018-07-10 17:43:23 +0900

    Replace power() with `<<` (bitwise left shift)

--- c/2018/2018-07-10-netmask.c
+++ c/2018/2018-07-10-netmask.c
@@ -1,20 +1,9 @@
 #include <stdio.h>
 
 int
-power(int a, int n) {
-	if (!n) {
-		return 1;
-	} else if (n&1) {
-		return a * power(a, n-1);
-	} else {
-		return power(a*a, n/2);
-	}
-}
-
-int
 main(void) {
 	for (int i = 1; i <= 32; ++i) {
-		int const mask = -power(2, 32-i);
+		int const mask = -1 << (32-i);
 		printf("%x\n", mask);
 	}
 	return 0;
