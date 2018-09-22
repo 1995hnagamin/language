7e2d828aaa51b9b24227eb03ac20c89f3082d891
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2018-09-22 21:06:53 +0900

    Remove cpp/2017/2017-08-17-temp-name
    Moved to sample-code/.

--- cpp/2017/2017-08-17-temp-name/9_2B.cpp
+++
@@ -1,9 +0,0 @@
-extern int count; // #1
-
-int lookup_example(int count) { // #2
-  if (count < 0) { // -> #2
-    int count = 1; // #3
-    lookup_example(count); // -> #3
-  }
-  return count /* #2 */ + ::count /* #1 */;
-}
--- cpp/2017/2017-08-17-temp-name/README.md
+++
@@ -1,1 +0,0 @@
-[C++テンプレート完全ガイド](http://www.shoeisha.co.jp/book/detail/9784798119229)
